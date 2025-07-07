63f3516d37ab5ef69c88e7fb3eb2cfd48852afd3
AP0me <anar.abdullazada@ufaz.az>
2025-07-07 17:00:16 +0400

    testing

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package tictactoe
 import(
 	"fmt"
 	"bufio"
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package tictactoe
+import(
+	"fmt"
+	// "bufio"
+	// "os"
+	// "strings"
+	// "strconv"
+)
+
+func test(){
+	fmt.Println("Hello World")
+}
